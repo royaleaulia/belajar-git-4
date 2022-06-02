b6146e7e86441c97a225cf17712a446d21553103
GF65 <royaleaulia@gmail.com>
2022-06-02 11:19:16 +0800

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, corrupti reiciendis consequuntur aperiam temporibus soluta numquam rem quibusdam impedit expedita. Voluptatum nisi at velit reprehenderit in. Architecto at accusamus voluptatibus!</p>
+</body>
+</html>
